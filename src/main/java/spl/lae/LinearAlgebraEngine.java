87e644dfceb30c8042cd8f7e0bab30221f1804d2
stavmo <stavmo@post.bgu.ac.il>
2025-12-29 17:13:39 +0000

    finished implementing LinearAlgebraEngine

--- src/main/java/spl/lae/LinearAlgebraEngine.java
+++ src/main/java/spl/lae/LinearAlgebraEngine.java
@@ -42,6 +42,49 @@ public class LinearAlgebraEngine {
     public void loadAndCompute(ComputationNode node) {
         // TODO: load operand matrices
         // TODO: create compute tasks & submit tasks to executor
+        ComputationNodeType type = node.getNodeType();
+        List<ComputationNode> children = node.getChildren();
+
+        double[][] left = children.get(0).getMatrix();
+        leftMatrix.loadRowMajor(left);
+
+        if(children.size() > 1){
+            double[][] right = children.get(1).getMatrix();
+            rightMatrix.loadRowMajor(right);
+        }
+
+        switch (type) {
+            case ADD:
+                createAddTasks();
+                break;
+            case MULTIPLY:
+                createMultiplyTasks();
+                break;
+            case NEGATE:
+                createNegateTasks();
+                break;
+            case TRANSPOSE:
+                createTransposeTasks();
+        
+            default:
+                throw new IllegalArgumentException("Unknown/unsupported computation type " + type);
+        }
+
+
+        List<Runnable> tasks = null;
+
+        executor.submitAll(tasks);
+        /* while(true){
+            try{
+                executor.shutdown();
+                break;
+            }
+            catch(new InterruptedException());
+        } */
+
+        double[][] result = leftMatrix.readRowMajor();
+        node.resolve(result);
+
     }
 
     public List<Runnable> createAddTasks() {
@@ -102,11 +145,38 @@ public class LinearAlgebraEngine {
 
     public List<Runnable> createTransposeTasks() {
         // TODO: return tasks that transpose rows
-        return null;
+        int rows = leftMatrix.length();
+        List<Runnable> tasks = new java.util.ArrayList<>();
+
+        for (int i = 0; i < rows; i++){
+            final int row = i;
+
+            tasks.add(() -> {
+                SharedVector vec = leftMatrix.get(row);
+                vec.transpose();
+            });
+        }
+        return tasks;
     }
 
     public String getWorkerReport() {
         // TODO: return summary of worker activity
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Worker Report:\n");
+
+        double[][] l = leftMatrix.readRowMajor();
+        sb.append(String.format("Left matrix: %d x %d%n", l.length, (l.length==0?0:l[0].length)));
+        
+        double[][] r = rightMatrix.readRowMajor();
+        sb.append(String.format("Right matrix: %d x %d%n", r.length, (r.length==0?0:r[0].length)));
+        
+        if (executor == null) {
+            sb.append("No executor available\n");
+        } 
+        else {
+            sb.append(executor.getWorkerReport()); // reuse the executor's detailed lines
+        }
+        return sb.toString();
+        
     }
 }
